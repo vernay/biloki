2bf807286540960e1ad719644cd5b8af8df80136
Grégoire Vernay <gregoire.vernay@icloud.com>
2026-02-06 16:43:06 +0100

    Retirer marketplace de la section

--- components/sections/FeaturesSection.tsx
+++ components/sections/FeaturesSection.tsx
@@ -31,10 +31,6 @@ const FEATURES: Feature[] = [
     translationKey: 'guideDigital',
   },
   {
-    id: 'marketplace',
-    translationKey: 'marketplace',
-  },
-  {
     id: 'multi-langue',
     translationKey: 'multiLangue',
   }
@@ -64,11 +60,6 @@ const iconMap: { [key: string]: ReactNode } = {
   'guide-digital': (
     <svg className="w-8 h-8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    </svg>
-  ),
-  marketplace: (
-    <svg className="w-8 h-8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
   ),
   'multi-langue': (
